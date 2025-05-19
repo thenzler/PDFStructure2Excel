365955dd80d1b9757dbf47dfb5a07ce7bf66c1f8
Thenzler <47194064+thenzler@users.noreply.github.com>
2025-05-19 10:07:23 +0200

    Hinzufügen der setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="PDFStructure2Excel",
+    version="1.0.0",
+    author="PDFStructure2Excel Contributors",
+    author_email="info@example.com",
+    description="Ein modernes Tool zur Konvertierung strukturierter PDF-Dokumente in Excel-Format",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thenzler/PDFStructure2Excel",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "PyQt5>=5.15.0",
+        "PyPDF2>=2.0.0",
+        "pandas>=1.3.0",
+        "openpyxl>=3.0.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "pdfstructure2excel=src.main:main",
+        ],
+    },
+)
